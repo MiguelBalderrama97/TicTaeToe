955af8b9b5ebd5883041ed08b16ca5d4a6a979b9
MiguelBalderrama97 <37118731+MiguelBalderrama97@users.noreply.github.com>
2019-09-24 18:33:50 -0600

    Generate Children

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Node n = new Node();
         System.out.println(n);
+        n.generateChildren();
+        n.printChildrens();
+       
     }
     
 }
--- src/Node.java
+++ src/Node.java
@@ -4,42 +4,55 @@ import java.util.ArrayList;
 
 public class Node {
     
-    private byte[] tictaetoe;
-    private byte turn;
-    private ArrayList<Node> childrens;
-    
-    private final int ARRAY_SIZE = 9;
+    private byte[] tictaetoe;               //(0)-VACIO     (1)-X's     (2)-O's
+    private byte turn;                      //(1)-X's   (2)-O's
+    private ArrayList<Node> children;
     
     public Node(){ //NODO INICIAL
-        this.tictaetoe = new byte[ARRAY_SIZE];
+        this.tictaetoe = new byte[9];
         for(int i: tictaetoe){
             tictaetoe[i] = 0;
         }
         this.turn = 1;
-        this.childrens = new ArrayList<>();
+        this.children = new ArrayList<>();
     }
     
     public Node(Node node){ 
         this.tictaetoe = new byte[9];
-        System.arraycopy(node.tictaetoe, 0, this.tictaetoe, 0, ARRAY_SIZE);
+        System.arraycopy(node.tictaetoe, 0, this.tictaetoe, 0, this.tictaetoe.length);
         this.turn = node.turn;
+        //this.turn = (byte)(node.turn == 1 ? 0 : 1);
     }
     
     public void generateChildren(){ // GENERAR TODOS LOS NODOS HIJOS Y ANAÑDIRLOS A LA LISTA CHILDREN
-        
+        byte market = (byte)(this.turn == 1 ? 1 : 0); //DEFINIR CUAL CARACTER SE ESCRIBIRA DEPENDIENDO DEL TURNO
+        for(int i = 0; i < this.tictaetoe.length; i++){
+            if(this.tictaetoe[i] == 0){ //COMPROBAR SI LA CASILLA ESTA VACIA
+                Node node = new Node(this); //CREAR UN NODO HIJO DE NUESTRO NODO ACTUAL
+                node.tictaetoe[i] = market; //SOBRESCRIBIR CON NUESTRO MARKET
+                node.turn = (byte)(this.turn == 1 ? 2 : 1); //CAMBIO DE TURNO
+                this.children.add(node); //AGREGAR EL NODO A LA LISTA CHILDREN
+            }
+        }
     }
 
     @Override
     public String toString() {
         // (X,0, , , , , , , ) Turn: X
         String  s = "{";
-        for(int i: tictaetoe){
-            s += this.tictaetoe[i] == 0 ? " " : this.tictaetoe[i] == 1 ? "X" : "O";
-            s += i < ARRAY_SIZE-1 ? "," : "";
+        for(int i = 0; i < this.tictaetoe.length; i++){
+            s += this.tictaetoe[i] == 0 ? " " : (this.tictaetoe[i] == 1 ? "X" : "O");
+            s += i < this.tictaetoe.length-1 ? "," : "";
         }
         s += "} Turn: ";
         s += this.turn ==  1 ? "X" : "O";
         return s;
+    }
+    
+    public void printChildrens(){
+        for(int i = 0; i < this.children.size(); i++){
+            System.out.println(this.children.get(i));
+        }
     }
     
     
